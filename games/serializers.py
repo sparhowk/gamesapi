e6309c000e46d12f2968e89905c8060d625037d2
Sparhowk <sparhowk@wp.pl>
2017-12-16 23:07:17 +0100

    First step
    add serializers, models

---
+++ games/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from games.models import Game
+
+
+class GameSerializers(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=200)
+    relase_date = serializers.DataTimeField()
+    game_category = serializers.CharField(max_length=200)
+    played = serializers.BooleanField(required=False)
+    
+    def create(self, validated_data):
+        return Game.object.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        
