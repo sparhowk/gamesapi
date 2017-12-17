274ac2752a46e05abc5bfd2795997061984eddfd
Sparhowk <sparhowk@wp.pl>
2017-12-17 21:59:41 +0100

    2017-12-17
    run server

--- games/serializers.py
+++ games/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from games.models import Game
 
 
-class GameSerializers(serializers.Serializer):
+class GameSerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=200)
-    relase_date = serializers.DataTimeField()
+    release_date = serializers.DateTimeField()
     game_category = serializers.CharField(max_length=200)
     played = serializers.BooleanField(required=False)
     
@@ -15,3 +15,6 @@ class GameSerializers(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.relase_date = validated_data('relase_date', instance.relase_date)
+        instance.game_category = validated_data('game_category', instance.game_category)
+        instance.played = validated_data('played', instance.played)
+        instance.save()
--- games/views.py
+++ games/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from games.models import Game
+from games.serializers import GameSerializer  
 
 # Create your views here.
