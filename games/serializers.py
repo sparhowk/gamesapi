d79c10d3642f403f42aa65625f4b083468ebb868
Sparhowk <sparhowk@wp.pl>
2017-12-27 22:50:18 +0100

    H.2017-12-27
    page 72

--- games/serializers.py
+++ games/serializers.py
@@ -2,19 +2,22 @@ from rest_framework import serializers
 from games.models import Game
 
 
-class GameSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=200)
-    release_date = serializers.DateTimeField()
-    game_category = serializers.CharField(max_length=200)
-    played = serializers.BooleanField(required=False)
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('id',
+                  'name',
+                  'release_date',
+                  'game_category',
+                  'played'
+                  )
 
     def create(self, validated_data):
         return Game.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
-        instance.relase_date = validated_data('relase_date', instance.relase_date)
+        instance.relase_date = validated_data('release_date', instance.release_date)
         instance.game_category = validated_data('game_category', instance.game_category)
         instance.played = validated_data('played', instance.played)
         instance.save()
